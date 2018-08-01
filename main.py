0b31fa6869461aed2613dde6db7a3d0d2c4fb098
mitsu9 <mitsunobu.h.9@gmail.com>
2018-08-01 18:34:52 +0900

    Create main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from sagemaker.tensorflow import TensorFlow
 
 def run():
-    tf_estimator = TensorFlow(entry_point='tf-keras-train.py', role='SageMakerRole',
-                            training_steps=10000, evaluation_steps=100,
-                            train_instance_count=1, train_instance_type='ml.p2.xlarge')
-    tf_estimator.fit('s3://bucket/path/to/training/data')
+    # sagemakerを使うためのインスタンス生成
+    # どのparamがrequiredでどれがoptionalかはまだ調べてないのでよくわかってない
+    # 一旦公式docsにあったやつを利用してる
+    tf_estimator = TensorFlow(
+        entry_point='tf-keras-train.py', # モデルなどについて書かれているファイル
+        role='SageMakerRole', # IAMロール. SageMakerが使える権限を与えておく必要がある
+        training_steps=20, # 学習時のパラメータ
+        evaluation_steps=10, # 学習時のパラメータ
+        train_instance_count=1, # 学習に使うインスタンスの個数. 複数指定して分散学習とかできる
+        train_instance_type='ml.m5.large' # インスタンスタイプ
+        )
 
-    tf_predictor = tf_estimator.deploy(initial_instance_count=1, instance_type='ml.c4.xlarge')
+    # 学習を開始する
+    tf_estimator.fit('s3://sagemaker-ap-northeast-1-192494425048') # s3のバケット名を与える
+
+    # 学習したモデルをデプロイする
+    tf_predictor = tf_estimator.deploy(initial_instance_count=1, instance_type='ml.t2.medium')
 
 if __name__ == '__main__':
     run()
